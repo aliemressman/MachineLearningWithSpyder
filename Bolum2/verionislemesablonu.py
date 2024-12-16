3e7e8fd879ef2a18325544370c6a0abc408fd370
Ali Emre Şişman <145591935+aliemressman@users.noreply.github.com>
2024-12-16 20:36:57 +0300

    Add files via upload
    Machine Learning -> 2

---
+++ Bolum2/verionislemesablonu.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  6 18:50:13 2020
+
+@author: aliem
+"""
+
+#1.kutuphaneler
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#2.veri onisleme
+#2.1.veri yukleme
+veriler = pd.read_csv('satislar.csv')
+#pd.read_csv("veriler.csv")
+#test
+print(veriler)
+
+#veri on isleme
+
+aylar = veriler[['aylar']]
+print(aylar)
+
+satislar = veriler[['satislar']]
+print(satislar)
+
+satislar2 = veriler.iloc[:,:1].values 
+print(satislar2)
+
+
+#verilerin egitim ve test icin bolunmesi
+from sklearn.model_selection import train_test_split
+
+x_train, x_test,y_train,y_test = train_test_split(aylar,satislar,test_size=0.33, random_state=0)
+
+#verilerin olceklenmesi
+from sklearn.preprocessing import StandardScaler
+
+sc=StandardScaler()
+
+X_train = sc.fit_transform(x_train)
+X_test = sc.transform(x_test)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
